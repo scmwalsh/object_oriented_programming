3e55868a37a3a73d96667b170307cfbaed7b86c5
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-10-29 14:39:00 -0400

    people.rb redo

--- people.rb
+++ people.rb
@@ -1,26 +1,47 @@
-class Person
-	attr_accessor :name
+#Question 1
+class Student
+
+end
+
+class Instructor
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+# class Person
+# 	attr_accessor :name
 	
-	def initialize(name)
-		@name = name
-		puts "Hi, my name is #{name}"
-	end
-end
+# 	def initialize(name)
+# 		@name = name
+# 		puts "Hi, my name is #{name}"
+# 	end
+# end
 
 
 
-class Student < Person
-	def learn
-		puts "I get it"
-	end
-end
+# class Student < Person
+# 	def learn
+# 		puts "I get it"
+# 	end
+# end
 
 
-class Instructor < Person
-	def teach
-		puts "everything in ruby is an object"
-		
-	end
-end
+# class Instructor < Person
+# 	def initialize
+# 		end
 
-name = Person.new("Chris")
+# 		def teach
+# 			puts "everthing is ruby is an object"
+# end
+
+# name = Person.new("Chris")
